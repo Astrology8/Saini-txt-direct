16fdabf031bc4db9b4b4c11472709df3401349be
Astrology8 <astrologyvideos8@gmail.com>
2025-06-05 18:21:22 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -6,9 +6,9 @@ from os import environ
 API_ID = int(environ.get("API_ID", "28325603"))
 API_HASH = environ.get("API_HASH", "4938d1018d2cb7a0c82103900594a97c")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "5680454765"))
+OWNER = int(environ.get("OWNER", ""))
 CREDIT = "𝄟⃝‌🐬🇳‌ɪᴋʜɪʟ𝄟⃝🐬"
-AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
